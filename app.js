e686db43ce7bf73706c349a139d08f46a16f9334
Bernard Arhia <55264725+everich4040@users.noreply.github.com>
2020-08-17 15:30:25 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,139 @@
+const $ = element => document.querySelector(element);
+const $All = element => document.querySelectorAll(element);
+const addBtn = $All(".plus");
+const total = $(".total-marks")
+const subtractBtn = $All(".minus");
+const userNames = $All(".username");
+const userVotes = $All(".votes");
+const userImages = $All(".user-img img");
+const progressBar = $(".progress");
+const LeaderboardImgs = $All(".leaderboard img");
+const LeaderboardNames = $All(".leaderboard .username");
+
+const counts = $All(".leaderboard span");
+
+const counters = { 
+    totalMarks:100,
+    users: [
+        {
+            name:"Erica",
+            votes:0,
+            id:0,
+            imgSrc: "./img/Erica.jpg"
+    },
+        {
+            name:"Lilo",
+            votes:0,
+            id:1,
+            imgSrc: "./img/Lilo.jpg"
+    },
+        {
+            name:"Ozo",
+            votes:0,
+            id:2,
+            imgSrc: "./img/Ozo.jpg"
+    },
+        {
+            name:"Prince",
+            votes:0,
+            id:3,
+            imgSrc: "./img/Prince.jpg"
+    },
+        {
+            name:"TrikyTee",
+            votes:0,
+            id:4,
+            imgSrc: "./img/TrikyTee.jpg"
+    },
+        {
+            name:"Vee",
+            votes:0,
+            id:5,
+            imgSrc: "./img/Vee.jpg"
+    },
+    ]
+}
+
+total.textContent = counters.totalMarks; 
+
+//Display username
+const displayUserInfo = ()=>{
+counters.users.map(user =>{
+    userNames.forEach((username, ElementIndex)=>{
+        user.id == ElementIndex ? username.textContent = user.name :"";
+     })
+     
+    userVotes.forEach((uservote, voteIndex)=>{
+        user.id == voteIndex ? uservote.textContent = user.votes :"";
+     })
+    userImages.forEach((userImg, imgIndex)=>{
+        user.id == imgIndex ? userImg.src = user.imgSrc :"";
+     })
+})
+}
+displayUserInfo()
+
+//Add vote to the user's score
+addBtn.forEach((btn, index)=>{
+   btn.addEventListener("click", ()=>{
+       counters.users.map((user,mapIndex,users) =>{
+           if(counters.totalMarks == 0 || user.votes == 100)return;
+           if(index === user.id){
+            users[index].votes++;
+            counters.totalMarks--;  
+            total.textContent = counters.totalMarks; 
+            progressBar.style.width = counters.totalMarks+"%";
+            }
+            userVotes.forEach((uservote, voteIndex)=>{
+                voteIndex == user.id ? uservote.textContent = user.votes :""
+             })
+           
+       })
+     
+})
+}
+)
+
+//Subtract vote to the user's score
+subtractBtn.forEach((btn, index)=>{
+   btn.addEventListener("click", ()=>{
+       counters.users.map((user,mapIndex,users) =>{
+        if(counters.totalMarks == 100 || user.votes == 0)return;
+           if(index === user.id){
+            counters.totalMarks++;
+                users[index].votes--;
+                total.textContent = counters.totalMarks;
+                progressBar.style.width = counters.totalMarks+"%"; 
+            }
+            userVotes.forEach((uservote, voteIndex)=>{
+                voteIndex == user.id ? uservote.textContent = user.votes :""
+             })
+           
+       })
+     
+})
+}
+)
+
+counts.forEach((count,index) =>{
+ 
+    count.innerHTML = index+1;
+})
+
+
+//Animate
+const viewBoard = $(".view");
+const backToVote = $(".back");
+const leaderboard = $(".leaderboard");
+const redirectToBoard = () =>{
+    leaderboard.style.opacity = 1;
+    leaderboard.style.pointerEvents = "auto";
+}
+const redirectToVote = () =>{
+    leaderboard.style.opacity = 0;
+    leaderboard.style.pointerEvents = "none";
+}
+
+viewBoard.onclick = redirectToBoard;
+backToVote.onclick = redirectToVote;
+
